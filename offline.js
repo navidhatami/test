2a806cd18ca9870fb697a4a9ddc73e066dfbe5af
نوید حاتمی <76665442+navidhatami@users.noreply.github.com>
2025-06-10 00:03:54 +0330

    upload complete

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1749492016,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/9patch.png",
+		"images/rostam-sheet0.png",
+		"images/rostam-sheet1.png",
+		"images/poll-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"images/sprite9-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/sprite10-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
